710574ced82ac4d79136625097f413bc344cd546
Sébastien Aupetit <sebtic@projectsforge.org>
2017-02-13 12:46:56 +0100

    Fix CI

--- defaultbuild-utils/src/utils/scripts/native-build-amd64-linux.sh
+++ defaultbuild-utils/src/utils/scripts/native-build-amd64-linux.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 export basedir=$(pwd)
-export CC=x86_64-pc-linux-gnu-gcc
-export CXX=x86_64-pc-linux-gnu-g++
-export CMAKE_SYSTEM_NAME=Linux
-export CMAKE_SYSTEM_PROCESSOR=x86_64
+#export CC=x86_64-pc-linux-gnu-gcc
+#export CXX=x86_64-pc-linux-gnu-g++
+#export CMAKE_SYSTEM_NAME=Linux
+#export CMAKE_SYSTEM_PROCESSOR=x86_64
 export buildsystem=default
 
 if [[ -f ../src/scripts/$(basename $0) ]]; then
--- defaultbuild-utils/src/utils/scripts/native-build-amd64-windows.sh
+++ defaultbuild-utils/src/utils/scripts/native-build-amd64-windows.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 export basedir=$(pwd)
-export CC=x86_64-pc-mingw32-gcc
-export CXX=x86_64-pc-mingw32-g++
-export CMAKE_SYSTEM_NAME=Windows
-export CMAKE_SYSTEM_PROCESSOR=x86_64
+#export CC=x86_64-pc-mingw32-gcc
+#export CXX=x86_64-pc-mingw32-g++
+#export CMAKE_SYSTEM_NAME=Windows
+#export CMAKE_SYSTEM_PROCESSOR=x86_64
 export buildsystem=default
 
 if [[ -f ../src/scripts/$(basename $0) ]]; then

fe71fed841ff2ae628237cffd394148d3dd79379
Sébastien Aupetit <sebtic@projectsforge.org>
2017-02-14 18:31:15 +0100

    Fix CI

--- build-utils/src/utils/scripts/std-build-script.sh
+++ build-utils/src/utils/scripts/std-build-script.sh
@@ -5,7 +5,7 @@ nativeos=$2
 
 # we are in /work/target
 for s in /work/src/scripts/native-build-${nativearch}-${nativeos}.sh /work/target/extracted/utils/scripts/cmake-build.sh; do
-	if [ -f $x ]; then
+	if [ -f $s ]; then
 	  echo "Found script $s"
 	  chmod a+x $s
 		$s

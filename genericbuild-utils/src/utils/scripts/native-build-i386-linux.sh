512715755c55e7aabaefb8f75277d426f34a7a0d
Sébastien Aupetit <sebtic@projectsforge.org>
2017-02-14 14:22:41 +0100

    Fix CI

--- genericbuild-utils/src/utils/scripts/native-build-i386-linux.sh
+++ genericbuild-utils/src/utils/scripts/native-build-i386-linux.sh
@@ -5,6 +5,9 @@ export CMAKE_SYSTEM_NAME=Linux
 export CMAKE_SYSTEM_PROCESSOR=i386
 export buildsystem=generic
 
+echo "CURRENT DIR : $(pwd)"
+echo "CONTENT : $(ls -al)"
+
 if [[ -f ../src/scripts/$(basename $0) ]]; then
   echo "Buindling with specific build scheme..."
   . ../src/scripts/$(basename $0)

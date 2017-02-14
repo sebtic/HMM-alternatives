755b2b3c9dd265f8ee7c6db141a8694ae8d65051
Sébastien Aupetit <sebtic@projectsforge.org>
2017-02-14 15:16:07 +0100

    Fix CI

--- build-utils/src/utils/scripts/native-build.sh
+++ build-utils/src/utils/scripts/native-build.sh
@@ -51,7 +51,7 @@ if  [ -f $SCRIPT ]; then
 	echo "Found build script $SCRIPT"
 	chmod +x $SCRIPT
 	echo "Running script with dockcross ..."
-	target/dockcross-${nativearch}-${nativeos} bash -c "cd target; bash $SCRIPT"
+target/dockcross-${nativearch}-${nativeos} bash -c "export HOME=/root; cd /work/target; ls -al; bash $SCRIPT"
 	result=$?
 	echo "Script has terminated"
 	exit $result
